738ea82ff233e181c5695f351094d1cc061e1c6e
Juri Schreib <juri@schreib.at>
2018-04-01 13:21:35 +0200

    Removed unused aliases

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -12,10 +12,8 @@ alias ssh='TERM=xterm-color ssh'
 alias pro="CDPATH=~/Sync/Projects cd"
 
 #Command substitutions
-alias 1pw=1password
+alias code="code-oss"
 #salias ssh="printf '\033]11;thistle1\007';ssh"
 
 #adding applications outside of PATH
-alias 1password="wine \"$HOME/.wine/drive_c/Program Files (x86)/1Password 4/1Password.exe\""
 alias racethesun="cd /opt/racethesun && ./RaceTheSun.x86"
-alias vscode="cd /opt/visual-studio-code/ && ./code"
